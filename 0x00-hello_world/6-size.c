38f39638e6afc916e49136fc522cee7cd39c26a5
lamia <lamiafaiz10@gmail.com>
2023-06-15 17:17:04 -0700

    commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main()
+{
+	printf("Size of a char: %u byte(s)\n",sizeof(char));
+	printf("Size of a int: %u byte(s)\n",sizeof(int));
+	printf("Size of a long int: %u byte(s)\n",sizeof(long int));
+	printf("Size of a long long: %u byte(s)\n",sizeof(long long));
+	printf("Size of a float: %u byte(s)\n",sizeof(float));
+	return (0);
+}
