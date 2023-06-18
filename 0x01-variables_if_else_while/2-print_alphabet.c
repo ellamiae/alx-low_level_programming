52d6d652064f1ec65ed97ef81caeb1cbb434cbaa
lamia <lamiafaiz10@gmail.com>
2023-06-18 12:46:12 -0700

    a

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: alphabet game
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+	return (0);
+}
