d4dffaa9a441143bb624c59c31c06b509915cb29
lamia <lamiafaiz10@gmail.com>
2023-06-19 14:25:16 -0700

    bet

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: lowercase e and q
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{int i;
+	for (i = 48 ; i <= 57 ; i++)
+	{putchar(i);
+	putchar(',');
+	putchar(' ');
+	}
+putchar('\n');
+return (0);
+}
