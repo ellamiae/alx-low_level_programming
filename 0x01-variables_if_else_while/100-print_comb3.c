3899009c6e2116f71324d0759b71498e36717912
lamia <lamiafaiz10@gmail.com>
2023-06-19 15:39:52 -0700

    a

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: combination of two digits
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{int i, j;
+	for (i = 48 ; i < 57 ; i++)
+	{
+		for (j = i + 1 ; j <= 57 ; j++)
+	{
+			putchar(i);
+		putchar(j);
+		putchar(',');
+		putchar(' ');
+		}
+	}
+putchar('\n');
+return (0);
+}
