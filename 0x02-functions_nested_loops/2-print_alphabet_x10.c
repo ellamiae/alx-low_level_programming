a078e27086f25b2d1ffa4b4af620a9630495cda4
lamia <lamiafaiz10@gmail.com>
2023-06-22 13:45:59 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_alphabet - x10
+ *
+*/
+
+void print_alphabet_x10(void)
+{int ch, i;
+for (i = 0; i < 10; i++)
+{
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar(ch);
+}
+_putchar('\n');
+}
