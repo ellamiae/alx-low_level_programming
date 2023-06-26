af3add73ff4bb83ff5736066d89696dc9e7b84a0
lamia <lamiafaiz10@gmail.com>
2023-06-26 16:10:21 -0700

    ptr

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaping two integers
+ *
+ * @a: parameter 1
+ * @b: parameter 2
+ *
+ * Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int x;
+	x = *a;
+	*a = *b;
+	*b = x;
+}
