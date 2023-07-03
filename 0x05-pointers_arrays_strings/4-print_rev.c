771b590f6c6d38338439ad076b66d56e66ecffca
lamia <lamiafaiz10@gmail.com>
2023-07-03 10:58:13 -0700

    ptr

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,10 @@
 */
 
 void print_rev(char *s)
-{	int i, len;
-	for (i = 0; s[i] != '\0'; s++)
-		len = len + 1;
-	for (i = len - 1; i >= 0; i--)
+{	int i = 0;
+	while (s[i])
+		i++;
+	while (i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
